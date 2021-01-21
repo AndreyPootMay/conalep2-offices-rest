ff1cb8181cab78ab98b13fa1f1a41c085ac2aea4
AndreyPootMay <spocker312@gmail.com>
2021-01-21 09:55:46 -0500

    Async/Await

--- src/routes/offices.js
+++ src/routes/offices.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { NULL } = require('mysql/lib/protocol/constants/types');
 const router = express.Router();
 
 const mysqlConnection = require('../database.js');
 
 // GET all Employees
-router.get('/offices', (req, res) => {
-    mysqlConnection.query('SELECT * FROM offices WHERE active = 1', (err, rows, fields) => {
+router.get('/offices', async (req, res) => {
+    await mysqlConnection.query('SELECT * FROM offices WHERE active = 1', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
@@ -16,11 +15,11 @@ router.get('/offices', (req, res) => {
 });
 
 // GET An Employee
-router.get('/offices/:id', (req, res) => {
+router.get('/offices/:id', async (req, res) => {
     const { id } = req.params;
-    mysqlConnection.query('SELECT * FROM offices WHERE id = ? AND active = 1', [id], (err, rows, fields) => {
+    await mysqlConnection.query('SELECT * FROM offices WHERE id = ? AND active = 1', [id], (err, rows, fields) => {
         if (!err) {
-            mysqlConnection.query('SELECT * FROM office_services WHERE office_id = ?', [id], (error, officeServices, fields) => {
+            await mysqlConnection.query('SELECT * FROM office_services WHERE office_id = ?', [id], (error, officeServices, fields) => {
                 if (!error) {
                     const office = rows[0];
                     if (officeServices != null) {
